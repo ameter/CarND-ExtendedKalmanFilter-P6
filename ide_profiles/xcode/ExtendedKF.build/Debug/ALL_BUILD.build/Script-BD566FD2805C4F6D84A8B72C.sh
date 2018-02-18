e8bed03e65914e6cb4d69a0a559dc75b7d1541d6
Chris Ameter <cameter@gmail.com>
2018-02-18 12:12:54 -0700

    cleaning up repo

--- ide_profiles/xcode/ExtendedKF.build/Debug/ALL_BUILD.build/Script-BD566FD2805C4F6D84A8B72C.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/chris/Developer/CarND-ExtendedKalmanFilter-P6/ide_profiles/xcode -f /Users/chris/Developer/CarND-ExtendedKalmanFilter-P6/ide_profiles/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- ide_profiles/xcode/ExtendedKF.build/Debug/ZERO_CHECK.build/Script-32FF10B3FE494A49BBF1BD17.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/chris/Developer/CarND-ExtendedKalmanFilter-P6/ide_profiles/xcode -f /Users/chris/Developer/CarND-ExtendedKalmanFilter-P6/ide_profiles/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
